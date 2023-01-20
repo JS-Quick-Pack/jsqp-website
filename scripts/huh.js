d2495b624724c6f1e85c4acc0ccb206f49476e13
Dev Goldy <jassim7256@gmail.com>
2023-01-20 16:08:00 +0000

    new branch

--- scripts/huh.js
+++ scripts/huh.js
@@ -1,5 +1,5 @@
 var huh_text = document.getElementById("huh_text")
-var huh_audio = new Audio('../cdn/audio/huh.mp3');
+var huh_audio = new Audio('./cdn/audio/huh.mp3');
 
 huh_text.addEventListener("click", function(event) {
     huh_audio.play();
